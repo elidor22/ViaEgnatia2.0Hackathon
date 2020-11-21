9488ad1b6aca169cae39d76347f14b9162794954
Siner <sinersakollri13@gmail.com>
2020-11-21 19:10:23 +0100

    Change to translator class. Info taken through toTranslate class and then used

---
+++ AzureCognitive/toTranslate.py
@@ -0,0 +1,14 @@
+# Functions to choose text to translate and language
+# Need to import class that contains chosen language and text (import nameOfClass)
+
+def gettext():
+    # text will be the element which contains the text regarding the monument
+    # text = className.variable
+    text = "See you"
+    return text
+
+
+def getlanguage():
+    # Same as for the function above
+    language = "it"
+    return language
