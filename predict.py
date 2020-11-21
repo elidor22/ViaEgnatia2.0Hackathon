b6ef4959201f24e35e8924a29ba5b1af156f00bb
elidor22 <elidorv@outlook.com>
2020-11-21 23:18:34 +0100

    Test save random file name predict.py

--- predict.py
+++ predict.py
@@ -14,83 +14,97 @@ import pickle
 import cv2
 import os
 import tensorflow as tf
+import random
+import string
+
 
 # construct the argument parser and parse the arguments
 from configPackage import config
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input", default='dataset/images/Church of Saint Mary in Apollonia/Kisha_e_Shën_Marisë_Apolloni.jpg',
-	help="path to input image/text file of image paths")
-args = vars(ap.parse_args())
+class predict:
+	def get_random_string(length):
+		letters = string.ascii_lowercase
+		result_str = ''.join(random.choice(letters) for i in range(length))
+		return result_str + '.jpg'
 
-# determine the input file type, but assume that we're working with
-# single input image
-filetype = mimetypes.guess_type(args["input"])[0]
-imagePaths = [args["input"]]
-tf.__version__
+	def predict(self):
+		ap = argparse.ArgumentParser()
+		ap.add_argument("-i", "--input",
+						default='dataset/images/Church of Saint Mary in Apollonia/Kisha_e_Shën_Marisë_Apolloni.jpg',
+						help="path to input image/text file of image paths")
+		args = vars(ap.parse_args())
 
-# if the file type is a text file, then we need to process *multiple*
-# images
-if "text/plain" == filetype:
-	# load the image paths in our testing file
-	imagePaths = open(args["input"]).read().strip().split("\n")
+		# determine the input file type, but assume that we're working with
+		# single input image
+		filetype = mimetypes.guess_type(args["input"])[0]
+		imagePaths = [args["input"]]
+		tf.__version__
 
-# load our object detector and label binarizer from disk
-print("[INFO] loading object detector...")
-with tf.device('/cpu:0'):
-	model = load_model('model.h5')
-#lb = pickle.loads(open(config.LB_PATH, "rb").read())
+		# if the file type is a text file, then we need to process *multiple*
+		# images
+		if "text/plain" == filetype:
+			# load the image paths in our testing file
+			imagePaths = open(args["input"]).read().strip().split("\n")
 
-# loop over the images that we'll be testing using our bounding box
-# regression model
-for imagePath in imagePaths:
-	# load the input image (in Keras format) from disk and preprocess
-	# it, scaling the pixel intensities to the range [0, 1]
-	image = load_img(imagePath, target_size=(224, 224))
-	image = img_to_array(image) / 255.0
-	image = np.expand_dims(image, axis=0)
+		# load our object detector and label binarizer from disk
+		print("[INFO] loading object detector...")
+		with tf.device('/cpu:0'):
+			model = load_model('model.h5')
+		# lb = pickle.loads(open(config.LB_PATH, "rb").read())
 
-	# predict the bounding box of the object along with the class
-	# label
-	with tf.device('/cpu:0'):
-		(boxPreds, labelPreds) = model.predict(image)
-	(startX, startY, endX, endY) = boxPreds[0]
+		# loop over the images that we'll be testing using our bounding box
+		# regression model
+		for imagePath in imagePaths:
+			# load the input image (in Keras format) from disk and preprocess
+			# it, scaling the pixel intensities to the range [0, 1]
+			image = load_img(imagePath, target_size=(224, 224))
+			image = img_to_array(image) / 255.0
+			image = np.expand_dims(image, axis=0)
 
-	# determine the class label with the largest predicted
-	# probability
-	i = np.argmax(labelPreds, axis=1)
-	#Convert i to int so there's no type mismatch
-	i = int(i)
-	print(i)
+			# predict the bounding box of the object along with the class
+			# label
+			with tf.device('/cpu:0'):
+				(boxPreds, labelPreds) = model.predict(image)
+			(startX, startY, endX, endY) = boxPreds[0]
 
-	#label = lb.classes_[i][0]
-	labels = ['church', 'monument', 'theatre']
-	print(i)
-	label = labels[i]
+			# determine the class label with the largest predicted
+			# probability
+			i = np.argmax(labelPreds, axis=1)
+			# Convert i to int so there's no type mismatch
+			i = int(i)
+			print(i)
+
+			# label = lb.classes_[i][0]
+			labels = ['church', 'monument', 'theatre']
+			print(i)
+			label = labels[i]
+
+			# load the input image (in OpenCV format), resize it such that it
+			# fits on our screen, and grab its dimensions
+			image = cv2.imread(imagePath)
+			image = imutils.resize(image, width=600)
+			(h, w) = image.shape[:2]
+
+			# scale the predicted bounding box coordinates based on the image
+			# dimensions
+			startX = int(startX * w)
+			startY = int(startY * h)
+			endX = int(endX * w)
+			endY = int(endY * h)
+
+			# draw the predicted bounding box and class label on the image
+			y = startY - 10 if startY - 10 > 10 else startY + 10
+			cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX,
+						0.65, (0, 255, 0), 2)
+			cv2.rectangle(image, (startX, startY), (endX, endY),
+						  (0, 255, 0), 2)
+			# show the output image
+			# Save the image that conmtains the bounding box
+			pred = predict
+			imgname = pred.get_random_string(12)
+			cv2.imwrite(imgname, image)
+			return
+predict.predict('dataset/images/theatre/unnamed.jpg')
 
 
-	# load the input image (in OpenCV format), resize it such that it
-	# fits on our screen, and grab its dimensions
-	image = cv2.imread(imagePath)
-	image = imutils.resize(image, width=600)
-	(h, w) = image.shape[:2]
 
-	# scale the predicted bounding box coordinates based on the image
-	# dimensions
-	startX = int(startX * w)
-	startY = int(startY * h)
-	endX = int(endX * w)
-	endY = int(endY * h)
-
-	# draw the predicted bounding box and class label on the image
-	y = startY - 10 if startY - 10 > 10 else startY + 10
-	cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (0, 255, 0), 2)
-	cv2.rectangle(image, (startX, startY), (endX, endY),
-		(0, 255, 0), 2)
-	# show the output image
-	cv2.imshow("Output", image)
-	#Save the image that conmtains the bounding box
-	cv2.imwrite('test.jpg', image)
-
-	cv2.waitKey(0)
