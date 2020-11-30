291cf8ee6a98266eafe0277a0260b736b2ebafcf
Arber Gjonaj <gjonajarber@yahoo.com>
2020-11-30 19:11:09 +0100

    Finishing touches done

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,33 +1,32 @@
-import React from "react"
-import ImageInput from "./ImageInput"
-import ProcessedImage from "./ProcessedImage"
-import Header from "../components/Header"
+import React from "react";
+import ImageInput from "./ImageInput";
+import ProcessedImage from "./ProcessedImage";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { connect } from "react-redux";
 
-class Main extends React.Component{
-    
-    render(){
-     const   checker=(yes)=>{
-if(yes===null){
-return <ImageInput/>
-}else return <ProcessedImage/>
-        }
-        return(
-            <div class="container text-center">
-                <Header/>
-{checker(this.props.fetchAIReduce)}
-            </div>
-        )
-    }
-    
+class Main extends React.Component {
+  render() {
+    const checker = (yes) => {
+      if (yes === null) {
+        return <ImageInput />;
+      } else return <ProcessedImage />;
+    };
+    return (
+      <div class="container text-center">
+        <Header />
+        {checker(this.props.fetchAIReduce)}
+        <Footer/>
+      </div>
+    );
+  }
 }
 
-
 const mapStateToProps = (state) => {
-    console.log(state);
-    return {
-      fetchAIReduce: state.fetchAI,
-    };
+  console.log(state);
+  return {
+    fetchAIReduce: state.fetchAI,
   };
-  
-  export default connect(mapStateToProps,)( Main);
+};
+
+export default connect(mapStateToProps)(Main);
